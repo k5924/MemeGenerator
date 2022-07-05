a11d6513d8ac6575f465c00965a65cca7003764e
k5924 <kamranbhatti03072001@gmail.com>
2022-07-05 13:41:28 +0100

    Subject: Random Meme Task: Return a random meme url from a data set List of Changes: - Created memeData set - Made a function in Meme to retrieve a random meme url from the data   set Note: next commit will involve updating state to display random meme from its url

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "../assets/css/meme.css";
+import memeData from "../data/memeData";
 
 export default function Meme(){
+  
+  function getMemeImage(){
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+  };
+
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" className="form--input" placeholder="Top Text"/>
         <input type="text" className="form--input" placeholder="Bottom Text"/>
-        <button className="form--button">Get a new meme image  🖼</button>
-      </form>
+        <button className="form--button" onClick={getMemeImage}>Get a new meme image  🖼</button>
+      </div>
     </main>
   );
 };
