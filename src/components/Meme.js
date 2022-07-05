4b6cd13e9fb3d448794cf8783f75b99c49f0d865
k5924 <kamranbhatti03072001@gmail.com>
2022-07-05 16:00:08 +0100

    Subject: Load Image Task: Load meme on button click using react state List of Changes: - Added state management in Meme componenet - Added css for image Note: Next commit will involve utilizing this state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,11 +3,12 @@ import "../assets/css/meme.css";
 import memeData from "../data/memeData";
 
 export default function Meme(){
+  const [memeImage, setMemeImage] = React.useState("");
   
   function getMemeImage(){
     const memesArray = memeData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
+    setMemeImage(memesArray[randomNumber].url);
   };
 
   return (
@@ -17,6 +18,7 @@ export default function Meme(){
         <input type="text" className="form--input" placeholder="Bottom Text"/>
         <button className="form--button" onClick={getMemeImage}>Get a new meme image  🖼</button>
       </div>
+        {memeImage && <img className="meme--image" src={memeImage} alt="meme"/>}
     </main>
   );
 };
