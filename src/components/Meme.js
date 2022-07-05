e23010ed53423a27547c76e8ffd0a4deb13c06ff
k5924 <kamranbhatti03072001@gmail.com>
2022-07-05 13:16:52 +0100

    Subject: Meme Form Task: Create a form for the content that would be displayed on a meme Solution: Created Meme component and styling Note: Next commit will involve learning about how to handle state

---
+++ src/components/Meme.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../assets/css/meme.css";
+
+export default function Meme(){
+  return (
+    <main>
+      <form className="form">
+        <input type="text" className="form--input" placeholder="Top Text"/>
+        <input type="text" className="form--input" placeholder="Bottom Text"/>
+        <button className="form--button">Get a new meme image  🖼</button>
+      </form>
+    </main>
+  );
+};
