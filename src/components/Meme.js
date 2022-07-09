5fcdf49a29bf2644de1ba106a344db224c3111db
k5924 <kamranbhatti03072001@gmail.com>
2022-07-09 15:01:21 +0100

    Subject: Memes API Problem: Memes were being retreived from local data store Solution: Created API request inside of Meme.js Note: Next commit will involve state and effect best practices

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../assets/css/meme.css";
-import memeData from "../data/memeData";
+
 
 export default function Meme(){
 
@@ -10,13 +10,18 @@ export default function Meme(){
     randomImage: ""
   });
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memeData);
+  const [allMemes, setAllMemes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res => res.json())
+    .then(data => setAllMemes(data.data.memes))
+  }, []);
 
   
   function getMemeImage(){
-    const memesArray = allMemeImages.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
     setMeme(prevMeme => ({
       ...prevMeme,
       randomImage: url
